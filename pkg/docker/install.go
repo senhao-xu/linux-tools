ec4cb95e6f43a4f2974fa42dcd4ba1ff496c0e36
senhao-xu <xusenhao1123@163.com>
2025-05-16 17:48:44 +0800

    update

--- pkg/docker/install.go
+++ pkg/docker/install.go
@@ -5,8 +5,29 @@ import (
 	"linux-tools/pkg/utils"
 )
 
+const (
+	daemon = `sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+    "registry-mirrors": [ 
+      "https://hub.senhao.xyz",
+      "https://docker.1ms.run"
+    ],
+    "log-opts": {
+      "max-size": "100m",
+      "max-file": "5"
+    }
+}
+EOF`
+)
+
 func Install(args []string) {
 	logrus.Infof("Install Docker.")
 
 	utils.ExecCmd("curl -fsSL https://get.docker.com | sudo bash -s docker --mirror Aliyun && sudo systemctl enable --now docker")
+
+	logrus.Infof("Start config image source.")
+	utils.ExecCmd(daemon)
+
+	utils.ExecCmd("sudo systemctl daemon-reload && sudo systemctl restart docker")
+
 }
